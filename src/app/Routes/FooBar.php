976e51a030f5f047d00b42e1604ba724779f01cf
Akai <akai-z@users.noreply.github.com>
2021-03-14 13:35:19 +0200

    Add an example route

---
+++ src/app/Routes/FooBar.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CustomComponentApp\App\Routes;
+
+use CustomComponentApp\Framework\Router\RouteInterface;
+use CustomComponentApp\Framework\Serialize\SerializerInterface;
+use Fig\Http\Message\RequestMethodInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class FooBar implements RouteInterface
+{
+    const HTTP_METHODS = [RequestMethodInterface::METHOD_GET];
+    const PATH = 'foobar';
+
+    private ResponseFactoryInterface $responseFactory;
+
+    public function __construct(SerializerInterface $serializer, ResponseFactoryInterface $responseFactory)
+    {
+        $this->serializer = $serializer;
+        $this->responseFactory = $responseFactory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function httpMethods(): array
+    {
+        return self::HTTP_METHODS;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function path(): string
+    {
+        return self::PATH;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $queryParams = $request->getQueryParams();
+        $bar = isset($queryParams['bar']) && $queryParams['bar'] !== '' ? $queryParams['bar'] : 'bar';
+
+        $response = $this->responseFactory->createResponse();
+        $response->getBody()->write($this->serializer->serialize(['foo' => $bar]));
+
+        return $response->withHeader('Content-type', 'application/json; charset=utf-8');
+    }
+}
